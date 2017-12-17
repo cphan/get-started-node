6e521889f6f6e70b0ccf9b445a436f256f781195
ibmadmin <cphan@us.ibm.com>
2017-12-17 09:13:22 -0500

    add Watson conversation cmas chatbot

--- server_ORIG.js
+++ server_ORIG.js
@@ -2,8 +2,6 @@ var express = require("express");
 var app = express();
 var cfenv = require("cfenv");
 var bodyParser = require('body-parser')
-var watson = require('watson-developer-cloud');
-var prompt = require('prompt-sync')();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -12,17 +10,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 var mydb;
-
-var conversation = watson.conversation({
-  username: '0a6a5518-6a26-450f-bfe6-b7bc0a4b7519',
-  password: 'v2wShXkq0wi7',
-  version: 'v1',
-  version_date: '2017-05-26'
-});
-
-var params = {
-  workspace_id: 'e2389e3c-d0f1-4c1d-a036-dd73b0ac1218'
-};
 
 /* Endpoint to greet and add a new visitor to database.
 * Send a POST request to localhost:3000/api/visitors with body
@@ -45,114 +32,6 @@ app.post("/api/visitors", function (request, response) {
     response.send("Hello " + userName + "! I added you to the database.");
   });
 });
-
-app.post("/api/cmas", function (request, response2) {
-  var userName = request.body.name;
-
-  conversation.message({
-    workspace_id: 'e2389e3c-d0f1-4c1d-a036-dd73b0ac1218',
-    input: {'text': userName}
-  },  function (err, response) {
-    if (err) {
-      console.error(err); // something went wrong
-      return;
-    }
-
-    // If an intent was detected, log it out to the console.
-    if (response.intents.length > 0) {
-      console.log('Detected intent: #' + response.intents[0].intent);
-    }
-
-    // If an entity was detected, log it out to the console.
-    if (response.entities.length > 0) {
-      console.log('Detected entity: #' + response.entities[0].entity);
-    }
-
-    // If a dialog was detected, log it out to the console.
-    if (response.dialog) {
-      console.log('Detected dialog: #' + response.dialog);
-    }
-
-
-    // Display the output from dialog, if any.
-    if (response.output.text.length != 0) {
-      //console.log(response.output.text.length);
-        // console.log(response.output.text[0]);
-        for (var i in response.output.text) {
-          console.log(response.output.text[i]);
-          response2.send("CMAS_Bot: " + response.output.text[i]);
-        }
-    }
-
-    // console.log(JSON.stringify(response, null, 2));
-
-    // Prompt for the next round of input.
-    // var newMessageFromUser = prompt('>> ');
-    //
-    // conversation.message({
-    //   workspace_id: 'e2389e3c-d0f1-4c1d-a036-dd73b0ac1218',
-    //   input: { text: newMessageFromUser }
-    //   }, processResponse)
-  }
-);
-
-
-
-  // if(!mydb) {
-  //   console.log("No database.");
-  //   response.send("Hello " + userName + " from Chris!");
-  //   return;
-  // }
-  // // insert the username as a document
-  // mydb.insert({ "name" : userName }, function(err, body, header) {
-  //   if (err) {
-  //     return console.log('[mydb.insert] ', err.message);
-  //   }
-  //   response.send("Hello " + userName + "! I added you to the database.");
-  // });
-});
-
-// Process the conversation response.
-function processResponse(err, response) {
-  if (err) {
-    console.error(err); // something went wrong
-    return;
-  }
-
-  // If an intent was detected, log it out to the console.
-  if (response.intents.length > 0) {
-    console.log('Detected intent: #' + response.intents[0].intent);
-  }
-
-  // If an entity was detected, log it out to the console.
-  if (response.entities.length > 0) {
-    console.log('Detected entity: #' + response.entities[0].entity);
-  }
-
-  // If a dialog was detected, log it out to the console.
-  if (response.dialog) {
-    console.log('Detected dialog: #' + response.dialog);
-  }
-
-
-  // Display the output from dialog, if any.
-  if (response.output.text.length != 0) {
-    //console.log(response.output.text.length);
-      // console.log(response.output.text[0]);
-      for (var i in response.output.text)
-        console.log(response.output.text[i]);
-  }
-
-  // console.log(JSON.stringify(response, null, 2));
-
-  // Prompt for the next round of input.
-  var newMessageFromUser = prompt('>> ');
-
-  conversation.message({
-    workspace_id: 'e2389e3c-d0f1-4c1d-a036-dd73b0ac1218',
-    input: { text: newMessageFromUser }
-    }, processResponse)
-}
 
 /**
  * Endpoint to get a JSON array of all the visitors in the database
